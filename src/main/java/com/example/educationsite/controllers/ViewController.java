f43ca7ce794eb6bbf076b278795689d0d72e7b8c
Antonis Fritzelas <antonyfrtz@gmail.com>
2024-09-27 19:42:07 +0300

    Move save logic to controller

--- src/main/java/com/example/educationsite/controllers/ViewController.java
+++ src/main/java/com/example/educationsite/controllers/ViewController.java
@@ -1,7 +1,6 @@
 package com.example.educationsite.controllers;
 
 import com.example.educationsite.dto.QuizSubmissionDTO;
-import com.example.educationsite.models.UserAnswer;
 import com.example.educationsite.services.QuizService;
 import com.example.educationsite.models.Quiz;
 import com.example.educationsite.models.QuizQuestion;
@@ -65,42 +64,9 @@ public class ViewController {
         List<QuizSubmissionDTO.AnswerDTO> answers = submission.getAnswers();
         List<QuizQuestion> questions = quizQuestionService.findByQuizId(quizId);
         UserEntity user = userService.findByUsername(username);
-
-        // Print quizId
-        System.out.println("Quiz ID: " + quizId);
         // Iterate through the questions
         for (QuizQuestion question : questions) {
-            Long questionId = question.getId();
-            String correctAnswer = question.getCorrectAnswer();
-
-            // Find the corresponding answer from the answers list
-            QuizSubmissionDTO.AnswerDTO submittedAnswer = answers.stream()
-                    .filter(answer -> answer.getQuestionId()==(questionId))
-                    .findFirst()
-                    .orElse(null);
-
-            UserAnswer userAnswer = answerService.findByUserAndQuestion(user, question);
-            if(userAnswer==null){ // first time answering
-                userAnswer = new UserAnswer();
-            }
-            userAnswer.setUserEntity(user);
-            userAnswer.setQuiz(question.getQuiz());
-            userAnswer.setQuizQuestion(question);
-
-            if (submittedAnswer != null) {
-                String providedAnswer = submittedAnswer.getAnswer();
-                boolean isCorrect = correctAnswer.equals(providedAnswer);
-                userAnswer.setCorrect(isCorrect);
-                // Print true if the answer is correct, false otherwise
-                System.out.println("Question ID: " + questionId);
-                System.out.println("Answer: " + providedAnswer);
-                System.out.println("Correct: " + isCorrect);
-            } else {
-                // If no answer was provided for this question, set isCorrect to false
-                userAnswer.setCorrect(false);
-                System.out.println("No answer provided for question ID: " + questionId);
-            }
-            answerService.saveUserAnswers(userAnswer);  // Assume there's a service to handle saving UserAnswer
+            answerService.saveUserAnswers(user,answers, question);  // Assume there's a service to handle saving UserAnswer
         }
 
         return ResponseEntity.status(HttpStatus.OK).body("Quiz submitted successfully.");
--- src/main/java/com/example/educationsite/services/UserAnswerService.java
+++ src/main/java/com/example/educationsite/services/UserAnswerService.java
@@ -1,5 +1,6 @@
 package com.example.educationsite.services;
 
+import com.example.educationsite.dto.QuizSubmissionDTO;
 import com.example.educationsite.models.QuizQuestion;
 import com.example.educationsite.models.UserAnswer;
 import com.example.educationsite.models.UserEntity;
@@ -9,15 +10,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserAnswerService {
 
     @Autowired
     private UserAnswerRepository userAnswerRepository;
 
-    public void saveUserAnswers(UserAnswer answer) {
+    public void saveUserAnswers(UserEntity user, List<QuizSubmissionDTO.AnswerDTO> answers, QuizQuestion question) {
+            Long questionId = question.getId();
+            String correctAnswer = question.getCorrectAnswer();
+
+            // Find the corresponding answer from the answers list
+            QuizSubmissionDTO.AnswerDTO submittedAnswer = answers.stream()
+                    .filter(answer -> answer.getQuestionId()==(questionId))
+                    .findFirst()
+                    .orElse(null);
+
+            UserAnswer userAnswer = this.findByUserAndQuestion(user, question);
+            if(userAnswer==null){ // first time answering
+                userAnswer = new UserAnswer();
+            }
+            userAnswer.setUserEntity(user);
+            userAnswer.setQuiz(question.getQuiz());
+            userAnswer.setQuizQuestion(question);
+
+            if (submittedAnswer != null) {
+                String providedAnswer = submittedAnswer.getAnswer();
+                boolean isCorrect = correctAnswer.equals(providedAnswer);
+                userAnswer.setCorrect(isCorrect);
+                // Print true if the answer is correct, false otherwise
+                System.out.println("Question ID: " + questionId);
+                System.out.println("Answer: " + providedAnswer);
+                System.out.println("Correct: " + isCorrect);
+            } else {
+                // If no answer was provided for this question, set isCorrect to false
+                userAnswer.setCorrect(false);
+                System.out.println("No answer provided for question ID: " + questionId);
+            }
             // Save the user's answer to the repository
-            userAnswerRepository.save(answer);
+            userAnswerRepository.save(userAnswer);
     }
 
     // Method to find existing UserAnswer by user and question
